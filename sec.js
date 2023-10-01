4a6a82ea877ee7d925a08456c306d9ceb16b1f82
AmerSmadii <amer.smadixx@gmail.com>
2023-10-01 19:01:58 +0300

    Done

--- sec.js
+++ sec.js
@@ -1,17 +1,36 @@
+
+let userAnswers =[];  
 let thename =window.prompt("what is your name","");
-let thegender =window.prompt("what is the Gander","");
-if(thegender=="male"){
-    window.alert(`welcome Mr ${thename} `);
-}else if (thegender=="female"){
-    window.alert(`welcome Ms ${thename} `);
-}
+userAnswers.push(thename);
+let gender ;
+function fun() {
+    
+    while (gender !== "male" || gender !== "female"){
+        gender=prompt("what is your gender").toLowerCase();
+     
+     if (gender =="male"){
+         window.alert(`welcome mr ${thename}!`);
+        userAnswers.push(gender);
+         break;
+     }
+     else if (gender =="female"){
+         window.alert(`welcome ms ${thename}!`);
+         userAnswers.push(gender);
+         break;}
+    }
+    }
+fun();
+  
+let drinkType =window.prompt("cold or hot","");
 
-else{
-    window.alert(`welcome  ${thename} `);
-}
-
-let cold =window.prompt("cold or hot","");
-
-let drink =window.prompt("enter your name's drink","");
+userAnswers.push(drinkType);
+let drinkName =window.prompt("enter your name's drink","");
+userAnswers.push(drinkName);
  window.alert(" Your Drink is getting prepared");
- console.log(`${thename} ${drink}`);
+
+
+ 
+
+ for (let i = 0; i < userAnswers.length; i++) {
+    console.log(userAnswers[i]);}
+  //csascas
